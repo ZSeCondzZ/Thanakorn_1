20bdd7d9bbae2a925292722c1a7525938ef1ece0
ZSeCondzZ <aimbotinwza007@gmail.com>
2024-11-04 10:09:25 +0700

    Add files via upload

---
+++ Term2/ทบทวน/app.js
@@ -0,0 +1,21 @@
+//let number1 = 'test'
+//let number2 = 'num'
+//let number3 = number1 + number2
+//console.log('New Number is', number3)
+
+//let number1 = 5
+//let number2 = 5
+//let condition1 = number1 >= number2
+//boolean (true, false)
+//console.log('result of condition1 is',condition1)
+//----------------------------------------------------------------------------------------------------------------------
+//if - else condition
+
+//if (number1 != number2) {
+//    console.log('this is if')}
+
+//else if(number1 == number2) {
+//    console.log('this is else if')}
+
+//else {
+//    console.log('this is else')}
