7d8c40424837c868e99216d8188eeafcd18be0c0
ZSeCondzZ <aimbotinwza007@gmail.com>
2024-11-07 13:28:46 +0700

    Add files via upload

--- Term2/ทบทวน/app.js
+++ Term2/ทบทวน/app.js
@@ -8,7 +8,9 @@
 //let condition1 = number1 >= number2
 //boolean (true, false)
 //console.log('result of condition1 is',condition1)
+
 //----------------------------------------------------------------------------------------------------------------------
+
 //if - else condition
 
 //if (number1 != number2) {
@@ -19,3 +21,57 @@
 
 //else {
 //    console.log('this is else')}
+
+//------------------------------------------------------------------------------------------------------------------------
+
+//ใช้ prompt สำหรับ input
+//prompt("ให้ใส่อะไร")
+//let hello = prompt('ใส่ข้อมูล')
+
+//Example
+
+//let score = prompt('ใส่คะแนน')
+//console.log('your score is ', score)
+//if (score >=80){
+//    console.log('เกรดที่ได้ : A')
+//}
+//else if (score >=70){
+//    console.log('เกรดที่ได้ : B')
+//}
+//else if (score >=60){
+//    console.log('เกรดที่ได้ : C')
+//}
+//else if (score >=50){
+//    console.log('เกรดที่ได้ : D')
+//}
+//else {
+//    console.log('เกรดที่ได้ : F')
+//}
+
+//------------------------------------------------------------------------------------------------------------------------
+
+//Conditional statement
+//&&,|| ,!
+//Loop (for,while)
+
+//for (let counter = 0; counter < 10; counter++) {
+//    console.log('hello world')
+//}
+
+//let text = ''
+//while(text ! = 'exit'){
+//    text = prompt('ใส่ข้อความมาใหม่')
+//}
+
+//------------------------------------------------------------------------------------------------------------------------
+
+//Array
+
+//let score1 = 50
+//let score2 = 60
+
+//let score = [60,50,40,10,90]
+//score = ['ll','ss']
+//score.push('tt')
+//score.pop()
+//console.log(score[ตำแหน่ง])
